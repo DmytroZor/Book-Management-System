025114eab0de4acbf10df79e498c1a32536a76a2
Дима Зоренко <dimasiork@gmail.com>
2025-09-13 13:07:56 +0300

    feat: update books, add routers to main, ad auth and limiter

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from app import models
 from app.db import engine
-from app.routers import books
+from app.routers import books, auth
 
 app = FastAPI()
 
@@ -12,6 +12,8 @@ async def init_models():
 @app.on_event("startup")
 async def on_startup():
     await init_models()
-
+app.include_router(auth.router)
 app.include_router(books.router)
 
+for route in app.routes:
+    print(route.path, route.methods)
